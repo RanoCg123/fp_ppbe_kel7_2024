e1db2fd16a3485b66e7d188aad53e67daaeed312
RanoCg123 <noumirano@gmail.com>
2024-06-11 14:07:43 +0700

    error handle

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fp_forum_kel7_ppbe/models/author_model.dart';
+import 'package:fp_forum_kel7_ppbe/widgets/top_bar.dart';
+import 'package:fp_forum_kel7_ppbe/controller/firebase_provider.dart';
+import '../models/author_model.dart';
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -10,7 +13,15 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final user = FirebaseAuth.instance.currentUser!;
-
+  void fetchCurrentUser() async {
+    FirebaseProvider userProvider = FirebaseProvider();
+    var currentUser = await userProvider.getCurrentUser();
+    if (currentUser != null) {
+      print('Current user: ${currentUser.name}');
+    } else {
+      print('No user logged in');
+    }
+  }
   // sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
@@ -19,11 +30,79 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: IconButton(
-          onPressed: signUserOut,
-          icon: const Icon(Icons.logout),
-        ),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            height: 160,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Profile",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: signUserOut,
+                        icon: Icon(Icons.logout, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(user.photoURL ?? ''),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            user.displayName ?? 'User Name',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            user.email ?? '',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35.0),
+                topRight: Radius.circular(35.0),
+              ),
+            ),
+            ),
+        ],
       ),
     );
   }
