c548321f05d82f993985bccfa72e274bdc25a174
Abhinaya Radiansyah Listiyanto <114990549+Abhinaya173@users.noreply.github.com>
2024-06-18 12:48:54 +0700

    Add files via upload

--- lib/services/reply_service.dart
+++ lib/services/reply_service.dart
@@ -3,51 +3,14 @@ import '../models/author_model.dart';
 import '../models/replies_model.dart';
 
 class FirestoreService {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final CollectionReference posts = FirebaseFirestore.instance.collection("posts");
+  final CollectionReference replies = FirebaseFirestore.instance.collection("replies");
+  final CollectionReference users = FirebaseFirestore.instance.collection("users");
 
-  Future<void> likeReply(String postId, String replyId, String userId) async {
-    final replyRef = _db.collection('posts').doc(postId).collection('replies').doc(replyId);
-
-    await _db.runTransaction((transaction) async {
-      final snapshot = await transaction.get(replyRef);
-      if (!snapshot.exists) {
-        throw Exception("Reply does not exist!");
-      }
-
-      final likedBy = List<String>.from(snapshot.data()!['likedBy'] ?? []);
-      if (!likedBy.contains(userId)) {
-        likedBy.add(userId);
-        transaction.update(replyRef, {
-          'likes': snapshot.data()!['likes'] + 1,
-          'likedBy': likedBy,
-        });
-      }
-    });
-  }
-
-  Future<void> unlikeReply(String postId, String replyId, String userId) async {
-    final replyRef = _db.collection('posts').doc(postId).collection('replies').doc(replyId);
-
-    await _db.runTransaction((transaction) async {
-      final snapshot = await transaction.get(replyRef);
-      if (!snapshot.exists) {
-        throw Exception("Reply does not exist!");
-      }
-
-      final likedBy = List<String>.from(snapshot.data()!['likedBy'] ?? []);
-      if (likedBy.contains(userId)) {
-        likedBy.remove(userId);
-        transaction.update(replyRef, {
-          'likes': snapshot.data()!['likes'] - 1,
-          'likedBy': likedBy,
-        });
-      }
-    });
-  }
 
   Future<Author?> getUserById(String uid) async {
     try {
-      DocumentSnapshot doc = await _db.collection('users').doc(uid).get();
+      DocumentSnapshot doc = await users.doc(uid).get();
       if (doc.exists) {
         return Author(
           name: doc['name'],
@@ -62,11 +25,12 @@ class FirestoreService {
     return null;
   }
 
-
-  Future<String> addReply(String questionId, Reply reply) async {
+  Future<String> addReply(
+      String questionId,
+      Reply reply
+      ) async {
     try {
-      DocumentReference docRef = _db
-          .collection('posts')
+      DocumentReference docRef = posts
           .doc(questionId)
           .collection('replies')
           .doc();
@@ -83,7 +47,7 @@ class FirestoreService {
         'created_at': reply.created_at,
         'likes': reply.likes,
       });
-      await _db.collection('posts').doc(questionId).update({
+      await posts.doc(questionId).update({
         'repliesCount': FieldValue.increment(1),
       });
       return reply.id;
@@ -93,42 +57,89 @@ class FirestoreService {
     }
   }
 
-  Future<void> updateReply(String questionId, String replyId, Reply reply) async {
-    await _db
-        .collection('posts')
+  Future<void> updateReply(
+      String questionId,
+      String replyId,
+      Reply reply
+      ) async {
+    await posts
         .doc(questionId)
         .collection('replies')
         .doc(replyId)
         .update(reply.toMap());
   }
 
-
-  Future<void> deleteReply(String questionId, String replyId) async {
+  Future<void> deleteReply(
+      String questionId,
+      String replyId
+      ) async {
     try {
-      await _db.collection('posts')
-          .doc(questionId)
-          .collection('replies')
-          .doc(replyId).delete();
-      await _db.collection('posts').doc(questionId).update({
+      await posts.doc(questionId).collection('replies').doc(replyId).delete();
+      await posts.doc(questionId).update({
         'repliesCount': FieldValue.increment(-1),
       });
     } catch (e) {
       print('Error deleting reply: $e');
-      throw e; // throw error untuk ditangkap di tempat pemanggilan
+      throw e; // Throw the error to be caught in the calling function
     }
   }
 
-  Future<List<Reply>> getReplies(String questionId) async {
-    var snapshot = await _db
-        .collection('posts')
+  Future<List<Reply>> getReplies(
+      String questionId
+      ) async {
+    var snapshot = await posts
         .doc(questionId)
         .collection('replies')
         .orderBy('created_at')
         .get();
-
     return snapshot.docs.map((doc) => Reply.fromMap(doc.data())).toList();
   }
 
-  updateReplyLikes(String id, String id2, int i) {}
+  Future<void> likeReply(
+      String postId,
+      String replyId,
+      String userId
+      ) async {
+    final replyRef = posts.doc(postId).collection('replies').doc(replyId);
 
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      final snapshot = await transaction.get(replyRef);
+      if (!snapshot.exists) {
+        throw Exception("Reply does not exist!");
+      }
+
+      final likedBy = List<String>.from(snapshot.data()!['likedBy'] ?? []);
+      if (!likedBy.contains(userId)) {
+        likedBy.add(userId);
+        transaction.update(replyRef, {
+          'likes': snapshot.data()!['likes'] + 1,
+          'likedBy': likedBy,
+        });
+      }
+    });
+  }
+
+  Future<void> unlikeReply(
+      String postId,
+      String replyId,
+      String userId
+      ) async {
+    final replyRef = posts.doc(postId).collection('replies').doc(replyId);
+
+    await FirebaseFirestore.instance.runTransaction((transaction) async {
+      final snapshot = await transaction.get(replyRef);
+      if (!snapshot.exists) {
+        throw Exception("Reply does not exist!");
+      }
+
+      final likedBy = List<String>.from(snapshot.data()!['likedBy'] ?? []);
+      if (likedBy.contains(userId)) {
+        likedBy.remove(userId);
+        transaction.update(replyRef, {
+          'likes': snapshot.data()!['likes'] - 1,
+          'likedBy': likedBy,
+        });
+      }
+    });
+  }
 }
